ae7d6b5dc151b78e4b3f5e86c263ed63445e43d0
Daniel Rodin <danielrodin@MacBook-Pro-sl-Daniel.local>
2025-11-04 22:59:52 +0200

    hw2

--- Cheers.java
+++ Cheers.java
@@ -17,6 +17,7 @@ public class Cheers {
                                 System.out.println("Give me an " +word.charAt(i)+ ": " +word.charAt(i)+ "!");
                 } else System.out.println("Give me a  " +word.charAt(i)+ ": " +word.charAt(i)+ "!");
             }
+            System.out.println("What does that spell?");
             for (int i = 0; i < times; i++) {
                 System.out.println(word+ " !!!");
             }
