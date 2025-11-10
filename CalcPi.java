fcc941fe15b3643c3d3d2e72d106ca7d7ee6c1d2
Daniel Rodin <danielrodin@MacBook-Pro-sl-Daniel.local>
2025-11-10 14:49:49 +0200

    hw done

--- CalcPi.java
+++ CalcPi.java
@@ -3,14 +3,12 @@ public class CalcPi {
 	public static void main(String [] args) { 
 	    int n = Integer.parseInt(args [0]);
 		double sum = 1;
-		int divide = 3;
 		for (int i = 1; i < n; i++) {
 			if (i % 2 != 0) {
-				sum -= 1.0 / divide;
-			} else sum += 1.0 / divide;
-			divide += 2;
+				sum -= 1.0 / (2 * i + 1);
+			} else sum += 1.0 / (2 * i + 1);
 		}
-		System.out.println("pi according to Java: " +Math.PI);
-		System.out.println("pi, approximated:     " +sum * 4);
+		System.out.println("pi according to Java: " + Math.PI);
+		System.out.println("pi, approximated:     " + sum * 4);
 	}
 }
